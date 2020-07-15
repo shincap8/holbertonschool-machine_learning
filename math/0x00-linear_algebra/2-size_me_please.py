6612648cdc62854fe74deeac1ef5716578116c6e
shincap8 <shincap8@gmail.com>
2020-07-15 13:56:23 +0000

    change comment of place

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-"""Function to return the shape of the matrix"""
-
-
 def matrix_shape(matrix):
+    """Function to return the shape of the matrix"""
     shape = []
     x = matrix
     while type(x) is list:
