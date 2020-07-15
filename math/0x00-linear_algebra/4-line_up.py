9002b33acd6c303420d8c58735f4a82dcc99add4
shincap8 <shincap8@gmail.com>
2020-07-15 14:22:58 +0000

    change condition to return None

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -2,13 +2,10 @@
 """Function to return the result of two arrange addition"""
 
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
-
-
 def add_arrays(arr1, arr2):
     """Function to return the result of two arrange addition"""
     add = []
-    if matrix_shape(arr1) != matrix_shape(arr2):
+    if len(arr1) != len(arr2):
         return None
     for i in range(len(arr1)):
         add.append(arr1[i] + arr2[i])
