dcee8aba55903086b7c6db52894cf8af28490882
shincap8 <shincap8@gmail.com>
2020-07-24 20:04:21 +0000

    make the list as small as possible

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -17,5 +17,8 @@ def poly_integral(poly, C=0):
                 ans.append(int(poly[i] / (i + 1)))
             else:
                 ans.append(poly[i] / (i + 1))
+        for i in range(len(ans) - 1, 0, -1):
+            if ans[i] == 0:
+                ans.pop()
         return ans
     return None
