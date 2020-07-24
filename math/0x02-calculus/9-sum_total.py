59c57a525e2aaf13b838c56682b7688b8a4255a7
shincap8 <shincap8@gmail.com>
2020-07-24 14:35:56 +0000

    Add some conditions for the None

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,8 +4,6 @@
 
 def summation_i_squared(n):
     """Function to calculate a summation"""
-    if type(n) is int or type(n) is float:
-        if ((n*(n+1)*((2*n)+1))/6) % 1 == 0:
-            return int((n*(n+1)*((2*n)+1))/6)
-        return (n*(n+1)*((2*n)+1))/6
+    if type(n) is int or n >= 0:
+        return int((n*(n+1)*((2*n)+1))/6)
     return None
