b4680c8154b321a5e45662bc578050f42bed99ad
shincap8 <shincap8@gmail.com>
2020-07-24 16:49:38 +0000

    add condition n > 0

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """Function to calculate a summation"""
-    if type(n) is int:
+    if type(n) is int and n > 0:
         return int((n*(n+1)*((2*n)+1))/6)
-    """return None"""
+    return None
