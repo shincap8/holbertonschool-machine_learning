50e8b9155f42c74202350ec873771197d0ccaf6c
shincap8 <shincap8@gmail.com>
2020-07-29 18:26:34 +0000

    try to fix output

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -7,19 +7,19 @@ class Binomial:
 
     def __init__(self, data=None, n=1, p=0.5):
         """class constructor"""
-        self.n = int(n)
-        self.p = float(p)
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
             if p <= 0 and p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
         else:
-            if type(data) != list:
+            if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
-            mean = sum(data) / len(data)
+            mean = sum(data) / int(len(data))
             dif = []
             for i in data:
                 dif.append((i - mean) ** 2)
@@ -32,15 +32,17 @@ class Binomial:
         """Calculates the value of the PMF for a given number of 'successes'"""
         if k <= 0:
             return 0
-        factor1, factor2, factor3 = 1, 1, 1
-        for i in range(1, (self.n + 1)):
-            factor1 = factor1 * i
-        for i in range(1, (k + 1)):
-            factor2 = factor2 * i
-        for i in range(1, (self.n - k + 1)):
-            factor3 = factor3 * i
-        return ((factor1 / (factor2 * factor3)) * (self.p ** k) *
-                ((1 - self.p) ** (self.n - k)))
+        if k <= self.n:
+            factor1, factor2, factor3 = 1, 1, 1
+            for i in range(1, (self.n + 1)):
+                factor1 = factor1 * i
+            for j in range(1, (k + 1)):
+                factor2 = factor2 * j
+            for l in range(1, (self.n - k + 1)):
+                factor3 = factor3 * l
+            return ((factor1 / (factor2 * factor3)) * (self.p ** k) *
+                    ((1 - self.p) ** (self.n - k)))
+        return 0
 
     def cdf(self, k):
         """Calculates the value of the CDF for a given number of 'successes'"""
