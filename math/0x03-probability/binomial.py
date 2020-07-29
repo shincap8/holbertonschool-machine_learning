795c32870d49d6a7df8d8869f422642cd52be990
shincap8 <shincap8@gmail.com>
2020-07-29 17:33:53 +0000

    Correct condition and remove minus 1

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -10,9 +10,9 @@ class Binomial:
         self.n = int(n)
         self.p = float(p)
         if data is None:
-            if n < 0:
+            if n <= 0:
                 raise ValueError("n must be a positive value")
-            if p < 0 and p > 1:
+            if p <= 0 and p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
         else:
             if type(data) != list:
@@ -23,12 +23,11 @@ class Binomial:
             dif = []
             for i in data:
                 dif.append((i - mean) ** 2)
-            var = sum(dif) / (len(data) - 1)
+            var = sum(dif) / len(data)
             p = 1 - (var / mean)
             oldn = mean / p
-            self.n = round(mean / p)
-            rate = oldn / self.n
-            self.p = p * rate
+            self.n = int(round(mean / p))
+            self.p = mean / self.n
 
     def pmf(self, k):
         """Calculates the value of the PMF for a given number of 'successes'"""
@@ -41,7 +40,8 @@ class Binomial:
             factor2 = factor2 * i
         for i in range(1, (self.n - k + 1)):
             factor3 = factor3 * i
-        return ((factor1 / (factor2 * factor3)) * (self.p ** k) * ((1 - self.p) ** (self.n - k)))
+        return ((factor1 / (factor2 * factor3)) * (self.p ** k) *
+                ((1 - self.p) ** (self.n - k)))
 
     def cdf(self, k):
         """Calculates the value of the CDF for a given number of 'successes'"""
