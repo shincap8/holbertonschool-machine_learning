6d9d350cf48d67e9b846853083c7e3c3e5a2a11f
shincap8 <shincap8@gmail.com>
2020-08-06 05:42:40 +0000

    Add condition to raise error

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -23,6 +23,8 @@ class DeepNeuralNetwork:
         self.weights["W1"] = np.random.randn(
             layers[0], nx)*np.sqrt(2/nx)
         self.weights["b1"] = np.zeros([layers[0], 1], dtype=float)
+        if layers[0] < 0:
+            raise TypeError("layers must be a list of positive integers")
         for i in range(1, self.L):
             if i < 0:
                 raise TypeError("layers must be a list of positive integers")
