c900f344e37521bc6396734b0ef629dd4b3c69c7
shincap8 <shincap8@gmail.com>
2020-08-11 17:33:19 +0000

    Add condition to return None

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -7,11 +7,13 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Function that converts a numeric label vector into a one-hot matrix"""
-    if len(Y.shape) == 1 and len(classes) > 2 and type(classes) is int:
-        hot = np.zeros([classes, Y.shape[0]])
-        for i in range(len(hot)):
-            for j in range(len(Y)):
-                if i == Y[j]:
-                    hot[i][j] = 1
-        return hot
-    return None
+    if len(Y.shape) == 0 or np.max(Y) > classes:
+        return None
+    if type(Y) is not np.ndarray or type(classes) is not int:
+        return None
+    hot = np.zeros([classes, Y.shape[0]])
+    for i in range(len(hot)):
+        for j in range(len(Y)):
+            if i == Y[j]:
+                hot[i][j] = 1
+    return hot
