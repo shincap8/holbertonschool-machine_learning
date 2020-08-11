3f8bd7c0ddad139ed77d794408e05de896226f69
shincap8 <shincap8@gmail.com>
2020-08-11 15:21:51 +0000

    remove try and except use of conditionals

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -7,14 +7,11 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """Function that converts a numeric label vector into a one-hot matrix"""
-    try:
-        Y.shape[1]
-        return None
-    except Exception:
-        C = np.amax(Y)
-        hot = np.zeros([C + 1, Y.shape[0]])
+    if len(Y.shape) == 1 and len(classes) > 2 and type(classes) is int:
+        hot = np.zeros([classes, Y.shape[0]])
         for i in range(len(hot)):
             for j in range(len(Y)):
                 if i == Y[j]:
                     hot[i][j] = 1
         return hot
+    return None
