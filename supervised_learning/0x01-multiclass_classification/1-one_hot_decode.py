6b59592f19e56bbd11dc2a5c62b30dafd038bcd0
shincap8 <shincap8@gmail.com>
2020-08-11 17:49:42 +0000

    Add condition to return None

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -7,8 +7,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """Function that converts a one-hot matrix into a vector of labels"""
-    try:
-        one_hot.shape[1]
-        return np.argmax(one_hot, axis=0)
-    except Exception:
+    if len(one_hot) == 0 or type(one_hot) is not np.ndarray:
         return None
+    if len(one_hot.shape) != 2:
+        return None
+    return np.argmax(one_hot, axis=0)
