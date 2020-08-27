367f4ecc09028876197cf568d066f4a05d40e781
shincap8 <shincap8@gmail.com>
2020-08-27 14:55:41 +0000

    change to sig to tahn, (that happens when you don't read)

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -9,12 +9,12 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Function that updates the weights and biases of a neural
     network using gradient descent with L2 regularization"""
-    dz = cache["A{}".format(L)] - Y
+    dz = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
-        L2 = (lambtha / Y.shape[1]) * weights["W{}".format(i)]
-        dw = (np.matmul(cache["A{}".format(i - 1)], dz.T) / Y.shape[1]) + L2.T
-        dz = np.matmul(weights["W{}".format(
-            i)].T, dz) * (cache["A{}".format(
-                i - 1)] * (1 - cache["A{}".format(i - 1)]))
-        weights["W{}".format(i)] = weights["W{}".format(
+        L2 = (lambtha / Y.shape[1]) * weights["W" + str(i)]
+        dw = (np.matmul(cache["A" + str(i - 1)], dz.T) / Y.shape[1]) + L2.T
+        dz = np.matmul(weights["W" + str(
+            i)].T, dz) * (1 - cache["A" + str(i - 1)]
+                          * cache["A" + str(i - 1)])
+        weights["W" + str(i)] = weights["W" + str(
             i)] - (alpha * dw).T
