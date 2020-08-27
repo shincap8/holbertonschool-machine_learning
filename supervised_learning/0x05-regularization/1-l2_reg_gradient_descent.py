24453c96e0b1f7f287e9392dc4531fdf0bf9b97c
shincap8 <shincap8@gmail.com>
2020-08-27 14:46:07 +0000

    don't touch bias just weights

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -11,7 +11,6 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     network using gradient descent with L2 regularization"""
     dz = cache["A{}".format(L)] - Y
     for i in range(L, 0, -1):
-        db = (np.sum(dz, axis=1, keepdims=True) / Y.shape[1])
         L2 = (lambtha / Y.shape[1]) * weights["W{}".format(i)]
         dw = (np.matmul(cache["A{}".format(i - 1)], dz.T) / Y.shape[1]) + L2.T
         dz = np.matmul(weights["W{}".format(
@@ -19,5 +18,3 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
                 i - 1)] * (1 - cache["A{}".format(i - 1)]))
         weights["W{}".format(i)] = weights["W{}".format(
             i)] - (alpha * dw).T
-        weights["b{}".format(i)] = weights["b{}".format(
-            i)] - (alpha * db)
