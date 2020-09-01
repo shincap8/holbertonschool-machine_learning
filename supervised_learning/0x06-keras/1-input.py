44d76771b4b652371d1e588b8a4e31533de6c267
shincap8 <shincap8@gmail.com>
2020-09-01 13:41:24 +0000

    correct when to stop loop

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -10,7 +10,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     l2 = K.regularizers.l2(lambtha)
     x = K.layers.Dense(layers[0], activation=activations[0],
                        kernel_regularizer=l2)(inputs)
-    for i in range(1, len(layers) - 1):
+    for i in range(1, len(layers)):
         x = K.layers.Dropout(1 - keep_prob)(x)
         x = K.layers.Dense(layers[i], activation=activations[i],
                            kernel_regularizer=l2)(x)
