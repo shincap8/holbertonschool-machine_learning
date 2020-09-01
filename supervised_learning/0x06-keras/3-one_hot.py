7e91ee68db689c3faf499371f9c5b8b8db5f5112
shincap8 <shincap8@gmail.com>
2020-09-01 13:22:11 +0000

    use classes in task 3, take the input and output outside the loop

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -7,4 +7,4 @@ import tensorflow.keras as K
 def one_hot(labels, classes=None):
     """Function that that converts a
     label vector into a one-hot matrix"""
-    return K.utils.to_categorical(labels)
+    return K.utils.to_categorical(labels, classes)
