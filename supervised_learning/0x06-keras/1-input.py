18846bee295b1a10f55a5657929b8ec5935cab65
shincap8 <shincap8@gmail.com>
2020-09-01 13:40:28 +0000

    loop start from 1

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -10,10 +10,9 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     l2 = K.regularizers.l2(lambtha)
     x = K.layers.Dense(layers[0], activation=activations[0],
                        kernel_regularizer=l2)(inputs)
-    for i in range(len(layers) - 1):
-        if i > 0 and i < len(layers) - 1:
-            x = K.layers.Dense(layers[i], activation=activations[i],
-                               kernel_regularizer=l2)(x)
+    for i in range(1, len(layers) - 1):
         x = K.layers.Dropout(1 - keep_prob)(x)
+        x = K.layers.Dense(layers[i], activation=activations[i],
+                           kernel_regularizer=l2)(x)
     model = K.Model(inputs=inputs, outputs=x)
     return model
