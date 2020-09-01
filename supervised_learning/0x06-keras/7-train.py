bf02e94f4473b9e61f3517ed75d65631314102fa
shincap8 <shincap8@gmail.com>
2020-09-01 15:13:36 +0000

    organize changes done in task 6 also here

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Function that trains a model using mini-batch gradient descent"""
+"""Function that trains a model using mini-batch gradient descent
+also analyze validaiton data, learning decay and early stopping"""
 
 import tensorflow.keras as K
 
@@ -8,19 +9,24 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, learning_rate_decay=False, alpha=0.1,
                 decay_rate=1, verbose=True, shuffle=False):
-    """Function that trains a model using mini-batch gradient descent"""
+    """return history"""
+    callback = []
     if validation_data is not None and early_stopping:
         callback = [K.callbacks.EarlyStopping(monitor="val_loss",
                                               patience=patience)]
-    else:
-        callback = NULL
     if validation_data is not None and learning_rate_decay:
         def lr_scheduler(epoch):
             return (alpha / (1 + (decay_rate * epoch)))
         learning = K.callbacks.LearningRateScheduler(schedule=lr_scheduler,
                                                      verbose=1)
         callback.append(learning)
-    history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, shuffle=shuffle, verbose=verbose,
-                          validation_data=validation_data, callbacks=callback)
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callback)
     return history
