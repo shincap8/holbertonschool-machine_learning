e331cd79ec788fabb745f6c95e4e3165fde2a4ab
shincap8 <shincap8@gmail.com>
2020-09-01 13:49:22 +0000

    dumb me remove assignment of model, correct that

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -16,4 +16,5 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                                kernel_regularizer=l2)(x)
         if i < len(layers) - 1:
             x = K.layers.Dropout(1 - keep_prob)(x)
+    model = K.Model(inputs=inputs, outputs=x)
     return model
