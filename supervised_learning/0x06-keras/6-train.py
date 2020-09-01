91babb2b51b0a2b4a58c2896d5bb1c91c6c4f818
shincap8 <shincap8@gmail.com>
2020-09-01 14:55:36 +0000

    label everything

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -7,13 +7,19 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
-    """Function that trains a model using mini-batch gradient descent"""
+    """return history"""
     if validation_data is not None and early_stopping:
         callback = [K.callbacks.EarlyStopping(monitor="val_loss",
                                               patience=patience)]
     else:
         callback = NULL
-    history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, shuffle=shuffle, verbose=verbose,
-                          validation_data=validation_data, callbacks=callback)
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callback)
     return history
