2c7068868232c8ee8a73fcd5568f8c8b05d81cd1
shincap8 <shincap8@gmail.com>
2020-09-01 13:00:48 +0000

    First layer inside loop

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -8,9 +8,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """Function that builds a neural network with the Keras library"""
     inputs = K.Input(shape=(nx,))
     l2 = K.regularizers.l2(lambtha)
-    x = K.layers.Dense(layers[0], activation=activations[0],
-                           kernel_regularizer=l2)(inputs)
     for i in range(len(layers)):
+        if i == 0:
+            x = K.layers.Dense(layers[0], activation=activations[0],
+                               kernel_regularizer=l2)(inputs)
         if i > 0 and i < len(layers) - 1:
             x = K.layers.Dense(layers[i], activation=activations[i],
                                kernel_regularizer=l2)(x)
