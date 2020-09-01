c8bbe0c4cc1945d00d61274e433ad147ee6cb98e
shincap8 <shincap8@gmail.com>
2020-09-01 14:16:47 +0000

    return directly

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Function that trains a model using mini-batch gradient descent"""
+"""Function that trains a model using mini-batch gradient descent
+also analyze validaiton data"""
 
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
-    """Function that trains a model using mini-batch gradient descent"""
-    history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, shuffle=shuffle, verbose=verbose,
-                          validation_data=validation_data)
-    return history
+    """Function that trains a model using mini-batch gradient descent
+    also analyze validaiton data"""
+    return network.fit(
+                       x=data, y=labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose,
+                       shuffle=shuffle, validation_data=validation_data)
