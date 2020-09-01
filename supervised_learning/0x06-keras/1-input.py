0993370c3e88ba7eddac8d30cadf49f99a71ef60
shincap8 <shincap8@gmail.com>
2020-09-01 13:38:17 +0000

    Not using output in Model

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -8,16 +8,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """Function that builds a neural network with the Keras library"""
     inputs = K.Input(shape=(nx,))
     l2 = K.regularizers.l2(lambtha)
-    dense = K.layers.Dense(layers[0], activation=activations[0],
-                           kernel_regularizer=l2)
-    x = dense(inputs)
+    x = K.layers.Dense(layers[0], activation=activations[0],
+                       kernel_regularizer=l2)(inputs)
     for i in range(len(layers) - 1):
         if i > 0 and i < len(layers) - 1:
             x = K.layers.Dense(layers[i], activation=activations[i],
                                kernel_regularizer=l2)(x)
         x = K.layers.Dropout(1 - keep_prob)(x)
-    outputs = K.layers.Dense(layers[len(layers) - 1],
-                             activation=activations[len(layers) - 1],
-                             kernel_regularizer=l2)(x)
-    model = K.Model(inputs=inputs, outputs=outputs)
+    model = K.Model(inputs=inputs, outputs=x)
     return model
