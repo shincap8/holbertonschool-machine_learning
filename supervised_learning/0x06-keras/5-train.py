4305c30002c424aeeb87652efabdf49db88c7c01
shincap8 <shincap8@gmail.com>
2020-09-01 14:20:51 +0000

    return History, label everything

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -5,11 +5,16 @@ also analyze validaiton data"""
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, verbose=True, shuffle=False):
-    """Function that trains a model using mini-batch gradient descent
-    also analyze validaiton data"""
-    return network.fit(
-                       x=data, y=labels, batch_size=batch_size,
-                       epochs=epochs, verbose=verbose,
-                       shuffle=shuffle, validation_data=validation_data)
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                verbose=True, shuffle=False):
+    """returns history"""
+    History = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data)
+    return History
