a8fa2d54760758eeb3d19ea24a753257ba98bdc6
shincap8 <shincap8@gmail.com>
2020-09-02 04:05:24 +0000

    return new model not none

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -14,7 +14,7 @@ def save_config(network, filename):
 
 def load_config(filename):
     """Function that  loads a model with a specific configuration"""
-    with open(filename, "r") as f:
+    with open(filename) as f:
         model = f.read()
     network = K.models.model_from_json(model)
-    return None
+    return network
