22af6251a56412403cbdf7c2c4388075b5196c9a
shincap8 <shincap8@gmail.com>
2020-09-02 04:02:07 +0000

    Task 11 with with

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -6,11 +6,15 @@ import tensorflow.keras as K
 
 def save_config(network, filename):
     """Function that saves a model’s configuration in JSON format"""
-    network.to_json(filename)
+    model = network.to_json()
+    with open(filename, "w") as f:
+        f.write(model)
     return None
 
 
 def load_config(filename):
     """Function that  loads a model with a specific configuration"""
-    network.from_json(filename)
+    with open(filename, "r") as f:
+        model = f.read()
+    network = K.models.model_from_json(model)
     return None
