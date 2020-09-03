c9e22c056149f35b790a5a6bab592affd6d5fe24
shincap8 <shincap8@gmail.com>
2020-09-03 20:39:09 +0000

    don't calculate the size of the convolved matrix

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -8,14 +8,12 @@ def convolve_grayscale_same(images, kernel):
     """Function that performs a valid convolution on grayscale images"""
     pw = int(kernel.shape[1] / 2)
     ph = int(kernel.shape[0] / 2)
-    nw = images.shape[2] - kernel.shape[1] + (2 * pw) + 1
-    nh = images.shape[1] - kernel.shape[0] + (2 * ph) + 1
-    convolved = np.zeros((images.shape[0], (nh), (nw)))
+    convolved = np.zeros((images.shape[0], images.shape[1], images.shape[2]))
     npad = ((0, 0), (ph, ph), (pw, pw))
     imagesp = np.pad(images, pad_width=npad,
                      mode='constant', constant_values=0)
-    for i in range(nh):
-        for j in range(nw):
+    for i in range(images.shape[1]):
+        for j in range(images.shape[2]):
             image = imagesp[:, i:i + kernel.shape[0],
                             j:j + kernel.shape[1]]
             convolved[:, i, j] = np.sum(np.multiply(image, kernel),
