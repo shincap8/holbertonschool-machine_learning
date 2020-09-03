500497bc8025415ec1d9273ade5f989d84de9b5e
shincap8 <shincap8@gmail.com>
2020-09-03 20:10:23 +0000

    change some functions

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -6,16 +6,16 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """Function that performs a valid convolution on grayscale images"""
-    nw = images.shape[2] - kernel.shape[1] + 1
-    nh = images.shape[1] - kernel.shape[0] + 1
     pw = int(kernel.shape[1] / 2)
     ph = int(kernel.shape[0] / 2)
-    convolved = np.zeros((images.shape[0], (nh + (2*ph)), (nw + (2*pw))))
+    nw = images.shape[2] - kernel.shape[1] + (2 * pw) + 1
+    nh = images.shape[1] - kernel.shape[0] + (2 * ph) + 1
+    convolved = np.zeros((images.shape[0], (nh), (nw))
     npad = ((0, 0), (1, ph), (1, pw))
     imagesp = np.pad(images, pad_width=npad,
                      mode='constant', constant_values=0)
-    for i in range(nh + (2*ph)):
-        for j in range(nw + (2*pw)):
+    for i in range(nh):
+        for j in range(nw):
             image = imagesp[:, i:(i + kernel.shape[0]),
                             j:(j + kernel.shape[1])]
             convolved[:, i, j] = np.sum(np.multiply(image, kernel),
