8b78c8337c11bb35ef16c3937b9da6412d8ad6db
shincap8 <shincap8@gmail.com>
2020-09-03 20:04:48 +0000

    2p, with another way of making calculation

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -8,15 +8,14 @@ def convolve_grayscale_same(images, kernel):
     """Function that performs a valid convolution on grayscale images"""
     nw = images.shape[2] - kernel.shape[1] + 1
     nh = images.shape[1] - kernel.shape[0] + 1
-    p = nw + kernel.shape[1] - images.shape[2]
-    convolved = np.zeros((images.shape[0], (nh + (2*p)), (nw + (2*p))))
-    imagesp = images
-    if p > 0:
-        npad = ((0, 0), (1, p), (1, p))
-        imagesp = np.pad(imagesp, pad_width=npad,
-                         mode='constant', constant_values=0)
-    for i in range(nh + (2*p)):
-        for j in range(nw + (2*p)):
+    pw = int(kernel.shape[1] / 2)
+    ph = int(kernel.shape[0] / 2)
+    convolved = np.zeros((images.shape[0], (nh + (2*ph)), (nw + (2*pw))))
+    npad = ((0, 0), (1, ph), (1, pw))
+    imagesp = np.pad(images, pad_width=npad,
+                     mode='constant', constant_values=0)
+    for i in range(nh + (2*ph)):
+        for j in range(nw + (2*pw)):
             image = imagesp[:, i:(i + kernel.shape[0]),
                             j:(j + kernel.shape[1])]
             convolved[:, i, j] = np.sum(np.multiply(image, kernel),
