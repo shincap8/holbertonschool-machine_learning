b4a707d0e0fbcf0732329421bd20a7804dd3c97f
shincap8 <shincap8@gmail.com>
2020-09-03 23:37:24 +0000

    task 4

---
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Function that performs a performs a convolution on images with channels"""
+
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Function that performs a convolution on images with channels"""
+    if padding == 'same':
+        ph = int(((images.shape[2] - 1) * stride[1] +
+                  kernel.shape[1] - images.shape[2]) / 2) + 1
+        ph = int(((images.shape[1] - 1) * stride[0] +
+                  kernel.shape[0] - images.shape[1]) / 2) + 1
+    elif padding == 'valid':
+        ph = 0
+        pw = 0
+    else:
+        pw = padding[1]
+        ph = padding[0]
+    nw = int(((images.shape[2] - kernel.shape[1] + (2 * pw)) / stride[1]) + 1)
+    nh = int(((images.shape[1] - kernel.shape[0] + (2 * ph)) / stride[0]) + 1)
+    convolved = np.zeros((images.shape[0], nh, nw))
+    npad = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+    imagesp = np.pad(images, pad_width=npad,
+                     mode='constant', constant_values=0)
+    for i in range(nh):
+        x = i * stride[0]
+        for j in range(nw):
+            y = j * stride[1]
+            image = imagesp[:, x:x + kernel.shape[0],
+                            y:y + kernel.shape[1], :]
+            convolved[:, i, j] = np.sum(np.multiply(image, kernel),
+                                        axis=(1, 2, 3))
+    return convolved
