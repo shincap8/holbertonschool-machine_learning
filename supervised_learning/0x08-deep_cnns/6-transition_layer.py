1da3a007be391d64d6f03826936d7fe3859e61e7
shincap8 <shincap8@gmail.com>
2020-09-16 20:47:35 +0000

    task 6

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Function that builds a transition layer as described
+in Densely Connected Convolutional Networks"""
+
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """Function that builds a transition layer as described
+    in Densely Connected Convolutional Networks"""
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    size = int(int(X.shape[3]) * compression)
+    X = K.layers.Conv2D(size, kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer='he_normal')(X)
+    X = K.layers.AvgPool2D((2, 2), padding='same')(X)
+    return (X, size)
