782c2fb4d3f8914b7edbd5334a6fea695d1063c7
shincap8 <shincap8@gmail.com>
2020-09-16 19:41:44 +0000

    Task 5

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -8,15 +8,19 @@ import tensorflow.keras as K
 def dense_block(X, nb_filters, growth_rate, layers):
     """Function that builds a dense block as described
     in Densely Connected Convolutional Networks"""
-    concatenate = inputs
+    concatenate = X
     for i in range(layers):
+        X = K.layers.BatchNormalization(axis=3)(concatenate)
+        X = K.layers.Activation('relu')(X)
+        X = K.layers.Conv2D(growth_rate * 4, kernel_size=(1, 1),
+                            padding='same',
+                            strides=(1, 1),
+                            kernel_initializer='he_normal')(X)
         X = K.layers.BatchNormalization(axis=3)(X)
         X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(64, kernel_size=(7, 7), padding='same',
-                            kernel_initializer='he_normal', strides=(2, 2))(inputs)
-        X = K.layers.BatchNormalization(axis=3)(X)
-        X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(64, kernel_size=(7, 7), padding='same',
-                            kernel_initializer='he_normal', strides=(2, 2))(inputs)
+        X = K.layers.Conv2D(growth_rate, kernel_size=(3, 3), padding='same',
+                            strides=(1, 1),
+                            kernel_initializer='he_normal')(X)
         concatenate = K.layers.concatenate([concatenate, X], axis=3)
-    return 
+        nb_filters = nb_filters + growth_rate
+    return (concatenate, nb_filters)
