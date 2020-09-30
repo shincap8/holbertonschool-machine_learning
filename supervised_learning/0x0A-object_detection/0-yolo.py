05208caea3afda3a0363e3c429c6268352ddb2e4
shincap8 <shincap8@gmail.com>
2020-09-30 22:44:09 +0000

    Task 0

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""class Yolo that uses the Yolo v3
+algorithm to perform object detection"""
+
+import tensorflow.keras as K
+
+
+class Yolo:
+    """Yolo Class"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """class constructor"""
+        model = K.models.load_model(model_path)
+        with open(classes_path) as f:
+            classes_t = f.readlines()
+        classes = [x.strip() for x in classes_t]
+        self.model = model
+        self.class_names = classes
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
