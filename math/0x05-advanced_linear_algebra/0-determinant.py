02954e70fa36c4cef9c8ad68addcdfef8c0606b4
shincap8 <shincap8@gmail.com>
2020-10-21 05:13:26 +0000

    Task 0 to 2

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Function that calculates the determinant of a matrix"""
+
+
+def determinant(matrix):
+    """Function that calculates the determinant of a matrix"""
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) > 0 and type(matrix[0]) is not list:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 0:
+        return 0
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1 and len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]))
+    det = []
+    for i in range(len(matrix)):
+        mini = [[j for j in matrix[i]] for i in range(1, len(matrix))]
+        for j in range(len(mini)):
+            mini[j].pop(i)
+        if i % 2 == 0:
+            det.append(matrix[0][i] * determinant(mini))
+        if i % 2 == 1:
+            det.append(-1 * matrix[0][i] * determinant(mini))
+    return sum(det)
