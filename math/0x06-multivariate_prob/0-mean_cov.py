816aaddd8e63b7ee8b4b2dfc219c83fd70d2b4ef
shincap8 <shincap8@gmail.com>
2020-10-26 17:53:37 +0000

    correct if condition to raise Type Error

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def mean_cov(X):
     """Function that calculates the mean and covariance of a data set"""
-    if type(X) is not np.ndarray:
+    if len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
