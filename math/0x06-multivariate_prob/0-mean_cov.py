a76b417e238a5188d459741c1054c304e916e992
shincap8 <shincap8@gmail.com>
2020-10-26 21:06:40 +0000

    Task 1

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -10,7 +10,7 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
-    meanx = np.zeros((1, X.shape[1]))
-    meanx[0] = np.mean(X, axis=0)
-    meany = np.dot(X.T, X - meanx) / (X.shape[0] - 1)
-    return (meanx, meany)
+    mean = np.zeros((1, X.shape[1]))
+    mean[0] = np.mean(X, axis=0)
+    C = np.dot(X.T, X - mean) / (X.shape[0] - 1)
+    return (mean, C)
