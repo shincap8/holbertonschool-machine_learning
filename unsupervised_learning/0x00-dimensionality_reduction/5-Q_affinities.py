cb07491ab6c1e17cb1726d569d758d1ea9ac4688
shincap8 <shincap8@gmail.com>
2020-10-29 18:23:45 +0000

    Task 5

--- unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -6,9 +6,9 @@ import numpy as np
 
 def Q_affinities(Y):
     """Function that calculates the Q affinities"""
-    sum_Y = np.sum(np.square(Y), 1)
+    sumY = np.sum(np.square(Y), 1)
     num = -2 * np.dot(Y, Y.T)
-    num = 1 / (1 + np.add(np.add(num, sum_Y).T, sum_Y))
+    num = 1 / (1 + np.add(np.add(num, sumY).T, sumY))
     num[range(Y.shape[0]), range(Y.shape[0])] = 0
     Q = num / np.sum(num)
     Q = np.maximum(Q, 1e-12)
