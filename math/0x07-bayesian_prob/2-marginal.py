11a1848178c82cc908f09bfa3bc556946fec5aeb
shincap8 <shincap8@gmail.com>
2020-10-30 23:43:36 +0000

    Task2

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""Function that calculates the likelihood of
-obtaining this data given various hypothetical
-probabilities of developing severe side effects"""
+""""Function that calculates the marginal
+probability of obtaining the data"""
 
 import numpy as np
 
 
-def likelihood(x, n, P):
-    """Function that calculates the likelihood of
-    obtaining this data given various hypothetical
-    probabilities of developing severe side effects"""
+def marginal(x, n, P, Pr):
+    """"Function that calculates the marginal
+    probability of obtaining the data"""
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
@@ -19,9 +17,17 @@ def likelihood(x, n, P):
         raise ValueError("x cannot be greater than n")
     if (not isinstance(P, np.ndarray)) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
+    if (not isinstance(Pr, np.ndarray)) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose([np.sum(Pr)], [1.])[0]:
+        raise ValueError("Pr must sum to 1")
     num = np.math.factorial(n)
     den = np.math.factorial(x) * np.math.factorial(n - x)
     coeficient = num / den
-    return coeficient * (P ** x) * ((1 - P) ** (n - x))
+    likehood = coeficient * (P ** x) * ((1 - P) ** (n - x))
+    intersection = likehood * Pr
+    return np.sum(intersection)
