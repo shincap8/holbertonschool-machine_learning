8acd3b2389d0887aa3c7dca1a5caf8b288c6bfa3
shincap8 <shincap8@gmail.com>
2020-11-03 16:44:47 +0000

    Add condition when k is 0

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -7,6 +7,8 @@ import numpy as np
 def initialize(X, k):
     """Function that initializes cluster centroids for K-means"""
     n, d = X.shape
+    if k == 0:
+        return None
     low = np.amin(X, axis=0)
     high = np.amax(X, axis=0)
     return np.random.uniform(low, high, size=(k, d))
