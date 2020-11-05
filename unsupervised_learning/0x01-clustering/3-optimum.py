fcc8b758ea63a2c8ec3602035927bfe4ce7f6aaf
shincap8 <shincap8@gmail.com>
2020-11-05 05:05:09 +0000

    Add som conditions to return None and for the kmax

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -10,20 +10,39 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """Function that tests for the optimum number of clusters by variance"""
     if type(X) is not np.ndarray:
         return (None, None)
-    if len(X.shape) != 2 or kmin < 0 or kmax < 0:
+    if type(kmin) is not int:
+        return (None, None)
+    if kmax is not None and type(kmax) is not int:
+        return (None, None)
+    if len(X.shape) != 2 or kmin < 0:
+        return (None, None)
+    if kmax is not None and kmax < kmin:
+        return (None, None)
+    if type(iterations) is not int:
         return (None, None)
     if iterations <= 0:
         return (None, None)
     results = []
     variances = []
-    for k in range(kmin, kmax + 1):
+    k = kmin
+    do = True
+    if kmax is None and X.shape[1] > kmin and X.shape[1] <= 10:
+        kmax = X.shape[0]
+        print(kmax)
+    while do:
         C, clss = kmeans(X, k)
         var = variance(X, C)
         results.append(C)
         results.append(clss)
         variances.append(var)
+        if kmax is not None and k == kmax:
+            do = False
+        if kmax is None and k == 10:
+            do = False
+        k = k + 1
     first = variances[0]
     d_vars = []
+    print(variances)
     for i in range(len(variances)):
         d_vars.append(first - variances[i])
     return (results, d_vars)
