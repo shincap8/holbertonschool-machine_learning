c3f725121aec10b9043e23a88e86d1c06f516880
shincap8 <shincap8@gmail.com>
2020-11-06 16:43:05 +0000

    Task 9

---
+++ unsupervised_learning/0x01-clustering/9-BIC.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Function that finds the best number of clusters
+for a GMM using the Bayesian Information Criterion"""
+
+import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
+
+
+def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
+    """Function that finds the best number of clusters
+    for a GMM using the Bayesian Information Criterion"""
+    if type(X) is not np.ndarray or type(kmin) is not int:
+        return (None, None, None, None)
+    if type(iterations) is not int or iterations <= 0:
+        return (None, None, None, None)
+    if kmax is None:
+        kmax = X.shape[0] - 1
+    if type(kmax) is not int or kmax <= 0 or kmax >= X.shape[0]:
+        return (None, None, None, None)
+    if kmin <= 0 or kmin >= X.shape[0] or kmin >= kmax:
+        return (None, None, None, None, None)
+    if type(tol) is not float or tol < 0:
+        return (None, None, None, None, None)
+    if type(verbose) is not bool:
+        return (None, None, None, None, None)
+    n, d = X.shape
+    ki = []
+    li = []
+    bi = []
+    tup = []
+    for k in range(kmin, kmax + 1):
+        pi, m, S, g, ll = expectation_maximization(X, k, iterations, tol, verbose)
+        p = (d * k) + (k * d * (d + 1) / 2) + k - 1
+        li.append(ll)
+        ki.append(k)
+        tup.append((pi, m, S))
+        BIC = p * np.log(n) - 2 * ll
+        bi.append(BIC)
+    ll = np.array(li)
+    b = np.array(bi)
+    top = np.argmin(b)
+    return (ki[top], tup[top], ll, b)
