5ec513caf615f617797b915df52d625cc82db0a5
shincap8 <shincap8@gmail.com>
2020-11-06 02:07:39 +0000

    Task 7

---
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Function that calculates the expectation
+step in the EM algorithm for a GMM"""
+
+import numpy as np
+
+
+def maximization(X, g):
+    """Function that calculates the expectation
+    step in the EM algorithm for a GMM"""
+    if type(X) is not np.ndarray or type(g) is not np.ndarray:
+        return (None, None, None)
+    if len(X.shape) != 2 or len(g.shape) != 2:
+        return (None, None, None)
+    n, d = X.shape
+    k, _ = g.shape
+    pi = g.sum(axis=1) / n
+    m = np.dot(g, X) / g.sum(1)[:, None]
+    S = np.zeros([k, d, d])
+    for i in range(k):
+        ys = X - m[i, :]
+        S[i] = (g[i, :, None, None] *
+                np.matmul(ys[:, :, None], ys[:, None, :])).sum(axis=0)
+    S = S / g.sum(axis=1)[:, None, None]
+    return (pi, m, S)
