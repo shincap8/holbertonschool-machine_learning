d6cca7fd7ad365700682fbc9fce7da1bd9cbf97c
shincap8 <shincap8@gmail.com>
2020-12-18 16:30:44 +0000

    Task 1 and change 0 using sklearn

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python3
 """Function that creates a bag of words embedding matrix"""
 
-import numpy as np
-import re
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
     """Function that creates a bag of words embedding matrix"""
-    words = []
-    if vocab is None:
-        for i in sentences:
-            sen = i.split(" ")
-            for s in sen:
-                word = re.split(r'\.|\!|\?|\,|\'', s)
-                if vocab is None:
-                    words.append(word[0].lower())
-        aux = set(words)
-        vocab = list(aux)
-        vocab.sort()
-    embeddings = np.zeros((len(sentences), len(vocab)))
-    for i in range(len(sentences)):
-        sentence = sentences[i].split(" ")
-        for s in sentence:
-            word = re.split(r'\.|\!|\?|\,|\'', s)
-            if word[0].lower() in vocab:
-                embeddings[i][vocab.index(word[0].lower())] += 1
-    return (embeddings.astype(int), vocab)
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return (X.toarray(), vectorizer.get_feature_names())
