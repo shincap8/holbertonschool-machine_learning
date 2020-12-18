5f5c2ee3ac7b0e91acdefb7aaaaefb8d426ddc34
shincap8 <shincap8@gmail.com>
2020-12-18 19:57:01 +0000

    Task 3

---
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function that converts a gensim word2vec model to a keras Embedding layer"""
+
+from gensim.models import Word2Vec
+
+
+def gensim_to_keras(model):
+    """Function that converts a gensim word2vec
+    model to a keras Embedding layer"""
+    layer = model.wv.get_keras_embedding()
+    return (layer)
