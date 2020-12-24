b32f404fbf9bd9a39c02775f1ae95a4780502d27
shincap8 <shincap8@gmail.com>
2020-12-24 16:22:04 +0000

    Pass True as parameter

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -7,5 +7,5 @@ from gensim.models import Word2Vec
 def gensim_to_keras(model):
     """Function that converts a gensim word2vec
     model to a keras Embedding layer"""
-    layer = model.wv.get_keras_embedding()
+    layer = model.wv.get_keras_embedding(True)
     return (layer)
