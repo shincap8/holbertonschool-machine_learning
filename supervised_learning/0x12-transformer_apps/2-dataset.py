4fd77103b0f2f9f5e6447b3a3b563a4c651ed13c
shincap8 <shincap8@gmail.com>
2021-01-13 19:04:52 +0000

    Change deprecated for features

--- supervised_learning/0x12-transformer_apps/2-dataset.py
+++ supervised_learning/0x12-transformer_apps/2-dataset.py
@@ -14,14 +14,14 @@ class Dataset:
         tokenizer_pt, tokenizer_en = self.tokenize_dataset(data_train)
         self.tokenizer_pt = tokenizer_pt
         self.tokenizer_en = tokenizer_en
-        self.data_train = data_train[0].map(self.tf_encode)
+        self.data_train = data_train.map(self.tf_encode)
         data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
                                split='validation', as_supervised=True)
         self.data_valid = data_valid.map(self.tf_encode)
 
     def tokenize_dataset(self, data):
         """Method that creates sub-word tokenizers for our dataset"""
-        subword = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus
+        subword = tfds.features.text.SubwordTextEncoder.build_from_corpus
         tokenizer_en = subword((en.numpy() for pt, en in data),
                                target_vocab_size=2**15)
         tokenizer_pt = subword((pt.numpy() for pt, en in data),
