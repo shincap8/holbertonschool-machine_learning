69b78f0c3c59622afe77dc9c41ec1a94deea4422
shincap8 <shincap8@gmail.com>
2021-01-13 21:04:40 +0000

    all in one

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -5,23 +5,15 @@ import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 
 
-def create_padding_mask(seq):
-    """Function to create padding"""
-    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
-    return seq[:, tf.newaxis, tf.newaxis, :]
-
-
-def create_look_ahead_mask(size):
-    """Function to mask future tokens"""
-    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-    return mask
-
-
 def create_masks(inputs, target):
     """Function that creates all masks for training/validation"""
-    enc_padding_mask = create_padding_mask(inputs)
+    enc_padding_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    enc_padding_mask = enc_padding_mask[:, tf.newaxis, tf.newaxis, :]
     dec_padding_mask = create_padding_mask(inputs)
-    look_ahead_mask = create_look_ahead_mask(tf.shape(target)[1])
+    dec_padding_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    dec_padding_mask = dec_padding_mask[:, tf.newaxis, tf.newaxis, :]
+    size = tf.shape(target)[1]
+    look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
     dec_target_padding_mask = create_padding_mask(target)
     combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
     return (enc_padding_mask, combined_mask, dec_padding_mask)
