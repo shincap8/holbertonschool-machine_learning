86ca802bb48770543d2a443564b357ce10d6cee0
shincap8 <shincap8@gmail.com>
2021-01-13 20:50:14 +0000

    Task 4

---
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Function that creates all masks for training/validation"""
+
+import tensorflow as tf
+
+
+def create_padding_mask(seq):
+    """Function to create padding"""
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+
+def create_look_ahead_mask(size):
+    """Function to mask future tokens"""
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+
+def create_masks(inputs, target):
+    """Function that creates all masks for training/validation"""
+    enc_padding_mask = create_padding_mask(inputs)
+    dec_padding_mask = create_padding_mask(inputs)
+    look_ahead_mask = create_look_ahead_mask(tf.shape(target)[1])
+    dec_target_padding_mask = create_padding_mask(target)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+    return (enc_padding_mask, combined_mask, dec_padding_mask)
