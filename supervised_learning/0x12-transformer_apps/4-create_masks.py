b426bda0367b1371a2e4e7cfd0a3920103510e65
shincap8 <shincap8@gmail.com>
2021-01-14 15:58:47 +0000

    You moron didn't delete the call to the functions eliminated

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 """Function that creates all masks for training/validation"""
 
-import tensorflow as tf
+import tensorflow.compat.v2 as tf
 
 
 def create_masks(inputs, target):
     """Function that creates all masks for training/validation"""
     enc_padding_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
     enc_padding_mask = enc_padding_mask[:, tf.newaxis, tf.newaxis, :]
-    dec_padding_mask = create_padding_mask(inputs)
     dec_padding_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
     dec_padding_mask = dec_padding_mask[:, tf.newaxis, tf.newaxis, :]
     size = tf.shape(target)[1]
     look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-    dec_target_padding_mask = create_padding_mask(target)
     combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
     return (enc_padding_mask, combined_mask, dec_padding_mask)
