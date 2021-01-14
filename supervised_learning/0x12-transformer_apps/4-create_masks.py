f39c6d68051b92b823e5d4d9621614d6c3fc20d5
shincap8 <shincap8@gmail.com>
2021-01-14 16:04:41 +0000

    include dec_target_p_mask erased in the last correction

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -10,7 +10,9 @@ def create_masks(inputs, target):
     enc_padding_mask = enc_padding_mask[:, tf.newaxis, tf.newaxis, :]
     dec_padding_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
     dec_padding_mask = dec_padding_mask[:, tf.newaxis, tf.newaxis, :]
-    size = tf.shape(target)[1]
+    size = target.shape[1]
     look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+    dec_target_p_mask = tf.cast(tf.math.equal(target, 0), tf.float32)
+    dec_target_p_mask = dec_target_p_mask[:, tf.newaxis, tf.newaxis, :]
+    combined_mask = tf.maximum(dec_target_p_mask, look_ahead_mask)
     return (enc_padding_mask, combined_mask, dec_padding_mask)
