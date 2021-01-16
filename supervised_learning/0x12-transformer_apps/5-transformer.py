1411672ef5d0acc3b8e16644159874d515d8a415
shincap8 <shincap8@gmail.com>
2021-01-16 13:40:37 +0000

    Task 5

---
+++ supervised_learning/0x12-transformer_apps/5-transformer.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+"""Transformer class o create the decoder for a transformer"""
+
+import tensorflow as tf
+
+def get_angles(pos, i, d_model):
+    """Angles"""
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+
+def positional_encoding(max_seq_len, dm):
+    """Function that calculates the positional encoding for a transformer"""
+    pos_encoding = get_angles(np.arange(max_seq_len)[:, np.newaxis],
+                              np.arange(dm)[np.newaxis, :],
+                              dm)
+    pos_encoding[:, 0::2] = np.sin(pos_encoding[:, 0::2])
+    pos_encoding[:, 1::2] = np.cos(pos_encoding[:, 1::2])
+    return pos_encoding
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """Function that calculates the scaled dot product attention"""
+    matmul_qk = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+    weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+    output = tf.matmul(weights, V)
+    return output, weights
+
+
+class EncoderBlock(tf.keras.layers.Layer):
+    """EncoderBlock class"""
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """Class constructor"""
+        super().__init__()
+        self.mha = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask=None):
+        """Call Method"""
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)
+        ffn = tf.keras.Sequential([
+            self.dense_hidden,
+            self.dense_output
+        ])
+        ffn_output = ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)
+        return out2
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """MultiHeadAttention class"""
+    def __init__(self, dm, h):
+        """Class constructor"""
+        super().__init__()
+        self.h = h
+        self.dm = dm
+        self.depth = int(dm / h)
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape
+        is (batch_size, num_heads, seq_len, depth)"""
+        x = tf.reshape(x, (batch_size, -1, self.h, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, Q, K, V, mask):
+        """Call Method"""
+        batch_size = tf.shape(Q)[0]
+        Q = self.Wq(Q)
+        K = self.Wk(K)
+        V = self.Wv(V)
+        Q = self.split_heads(Q, batch_size)
+        K = self.split_heads(K, batch_size)
+        V = self.split_heads(V, batch_size)
+        output, weights = sdp_attention(Q, K, V, mask)
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(output,
+                                      (batch_size, -1, self.dm))
+        output = self.linear(concat_attention)
+        return (output, weights)
+
+
+class DecoderBlock(tf.keras.layers.Layer):
+    """DecoderBlock class"""
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """Class constructor"""
+        super().__init__()
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """Call Method"""
+        attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(attn1 + x)
+        attn2, attn_weights_block2 = self.mha2(out1, encoder_output,
+                                               encoder_output, padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(attn2 + out1)
+        ffn = tf.keras.Sequential([
+            self.dense_hidden,
+            self.dense_output
+        ])
+        ffn_output = ffn(out2)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layernorm3(ffn_output + out2)
+        return out3
+
+
+class Decoder(tf.keras.layers.Layer):
+    """Decoder class"""
+    def __init__(self, N, dm, h, hidden, target_vocab,
+                 max_seq_len, drop_rate=0.1):
+        """Class constructor"""
+        super().__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """Call Method"""
+        seq_len = x.shape[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:seq_len, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.N):
+            x = self.blocks[i](x, encoder_output, training,
+                               look_ahead_mask, padding_mask)
+        return x
+
+
+class Encoder(tf.keras.layers.Layer):
+    """Encoder class"""
+    def __init__(self, N, dm, h, hidden, input_vocab,
+                 max_seq_len, drop_rate=0.1):
+        """Class constructor"""
+        super().__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """Call Method"""
+        seq_len = x.shape[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:seq_len, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.N):
+            x = self.blocks[i](x, training, mask)
+        return x
+
+
+class Transformer(tf.keras.Model):
+    """Transformer class"""
+    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
+                 max_seq_input, max_seq_target, drop_rate=0.1):
+        """Class constructor"""
+        super().__init__()
+        self.encoder = Encoder(N, dm, h, hidden, input_vocab,
+                               max_seq_input, drop_rate)
+        self.decoder = Decoder(N, dm, h, hidden, target_vocab,
+                               max_seq_target, drop_rate)
+        self.linear = tf.keras.layers.Dense(target_vocab)
+
+    def call(self, inputs, target, training, encoder_mask,
+             look_ahead_mask, decoder_mask):
+        """Call Method"""
+        enc_output = self.encoder(inputs, training, encoder_mask)
+        dec_output = self.decoder(target, enc_output, training,
+                                  look_ahead_mask, decoder_mask)
+        final_output = self.linear(dec_output)
+        return final_output
