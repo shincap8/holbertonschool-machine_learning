37113420dc06ad2752c1d0bd810f98637f1445db
shincap8 <shincap8@gmail.com>
2021-02-14 22:22:12 +0000

    correct spaghetti code

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -6,18 +6,35 @@ import numpy as np
 
 
 def convolve_grayscale_padding(images, kernel, padding):
-    """Function that performs a valid convolution
-    on grayscale images with custom padding"""
-    nw = int(images.shape[2] - kernel.shape[1] + (2 * padding[1]) + 1)
-    nh = int(images.shape[1] - kernel.shape[0] + (2 * padding[0]) + 1)
-    convolved = np.zeros((images.shape[0], nh, nw))
-    npad = ((0, 0), (padding[0], padding[0]), (padding[1], padding[1]))
+    """Performs a convolution on grayscale images custom padding
+    Args:
+        images: `numpy.ndarray` with shape (m, h, w)
+            containing multiple grayscale images
+            m: `int`, is the number of images
+            h: `int`, is the height in pixels of the images
+            w: `int`, is the width in pixels of the images
+        kernel: `numpy.ndarray` with shape (kh, kw)
+            containing the kernel for the convolution
+            kh: `int`, is the height of the kernel
+            kw: `int`, is the width of the kernel
+        padding: `tuple` of (ph, pw)
+            ph: `int` is the padding for the height of the image
+            pw: `int` is the padding for the width of the image
+    Returns:
+        output: `numpy.ndarray` containing the convolved images
+    """
+    m, h, w = images.shape[0], images.shape[1], images.shape[2]
+    kh, kw = kernel.shape[0], kernel.shape[1]
+    ph, pw = padding[0], padding[1]
+    nw = int(w - kw + (2 * pw) + 1)
+    nh = int(h - kh + (2 * ph) + 1)
+    convolved = np.zeros((m, nh, nw))
+    npad = ((0, 0), (ph, ph), (pw, pw))
     imagesp = np.pad(images, pad_width=npad,
                      mode='constant', constant_values=0)
     for i in range(nh):
         for j in range(nw):
-            image = imagesp[:, i:i + kernel.shape[0],
-                            j:j + kernel.shape[1]]
+            image = imagesp[:, i:i + kh, j:j + kw]
             convolved[:, i, j] = np.sum(np.multiply(image, kernel),
                                         axis=(1, 2))
     return convolved
--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -6,31 +6,52 @@ import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """Function that performs a valid convolution
-    on grayscale images with custom padding"""
+    """Performs a convolution on grayscale images
+    Args:
+        images: `numpy.ndarray` with shape (m, h, w)
+            containing multiple grayscale images
+            m: `int`, is the number of images
+            h: `int`, is the height in pixels of the images
+            w: `int`, is the width in pixels of the images
+        kernel: `numpy.ndarray` with shape (kh, kw)
+            containing the kernel for the convolution
+            kh: `int`, is the height of the kernel
+            kw: `int`, is the width of the kernel
+        padding: `tuple` of (ph, pw), ‘same’, or ‘valid’
+            if `tuple`:
+                ph: `int` is the padding for the height of the image
+                pw: `int` is the padding for the width of the image
+            if ‘same’, performs a same convolution
+            if ‘valid’, performs a valid convolution
+        stride is a tuple of (sh, sw)
+            sh: `int`, is the stride for the height of the image
+            sw: `int`, is the stride for the width of the image
+    Returns:
+        output: `numpy.ndarray` containing the convolved images
+    """
+    m, h, w = images.shape[0], images.shape[1], images.shape[2]
+    kh, kw = kernel.shape[0], kernel.shape[1]
+    sh, sw = stride[0], stride[1]
     if padding == 'same':
-        ph = int(((images.shape[2] - 1) * stride[1] +
-                  kernel.shape[1] - images.shape[2]) / 2) + 1
-        pw = int(((images.shape[1] - 1) * stride[0] +
-                  kernel.shape[0] - images.shape[1]) / 2) + 1
+        ph = int(((w - 1) * sw + kw - w) / 2) + 1
+        pw = int(((h - 1) * sh + kh - h) / 2) + 1
     elif padding == 'valid':
         ph = 0
         pw = 0
     else:
         pw = padding[1]
         ph = padding[0]
-    nw = int(((images.shape[2] - kernel.shape[1] + (2 * pw)) / stride[1]) + 1)
-    nh = int(((images.shape[1] - kernel.shape[0] + (2 * ph)) / stride[0]) + 1)
-    convolved = np.zeros((images.shape[0], nh, nw))
+    nw = int(((w - kw + (2 * pw)) / sw) + 1)
+    nh = int(((h - kh + (2 * ph)) / sh) + 1)
+    convolved = np.zeros((m, nh, nw))
     npad = ((0, 0), (ph, ph), (pw, pw))
     imagesp = np.pad(images, pad_width=npad,
                      mode='constant', constant_values=0)
     for i in range(nh):
         x = i * stride[0]
         for j in range(nw):
-            y = j * stride[1]
-            image = imagesp[:, x:x + kernel.shape[0],
-                            y:y + kernel.shape[1]]
+            y = j * sw
+            image = imagesp[:, x:x + kh, y:y + kw]
             convolved[:, i, j] = np.sum(np.multiply(image, kernel),
                                         axis=(1, 2))
     return convolved
