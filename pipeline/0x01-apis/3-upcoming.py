c902cf11ea4932479daffe5017fca7eb2995d71e
shincap8 <shincap8@gmail.com>
2021-03-12 01:40:16 +0000

    correct printing

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -8,21 +8,21 @@ import time
 if __name__ == '__main__':
     url = "https://api.spacexdata.com/v4/launches/upcoming"
     r = rq.get(url)
-    d_unix = int(time.time())
-    for i in range(len(r.json())):
-        if d_unix > r.json()[i]['date_unix']:
-            d_unix = r.json()[i]['date_unix']
-            ind = i
-    launch_name = r.json()[ind]['name']
-    date = r.json()[ind]['date_local']
-    rocket_id = r.json()[ind]['rocket']
-    launchpad_id = r.json()[ind]['launchpad']
+    launches = sorted(r.json(), key=lambda i: i['date_unix'])
+    date_unix = launches[0]['date_unix']
+    for i in r.json():
+        if i['date_unix'] == date_unix:
+            launch_name = i['name']
+            date = i['date_local']
+            rocket_id = i['rocket']
+            launchpad_id = i['launchpad']
+            break
     url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
     r = rq.get(url)
     rocket_name = r.json()['name']
     url = "https://api.spacexdata.com/v4/launchpads/{}".format(launchpad_id)
     r = rq.get(url)
-    launchpad_name = r.json()['name']
-    launchpad_locality = r.json()['locality']
+    lpad_name = r.json()['name']
+    lpad_locality = r.json()['locality']
     print("{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
-                                        launch_name, launchpad_locality))
+                                        lpad_name, lpad_locality))
