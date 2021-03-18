cbe4a1a2845eeeb7d62cdabf0b316f47660aa728
shincap8 <shincap8@gmail.com>
2021-03-17 21:12:37 -0500

    pleasing the checker

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -1,6 +1,16 @@
---script that creates a table users following these requirements:
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
---name, string (255 characters)
---country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
-CREATE TABLE IF NOT EXISTS users(id INTEGER NOT NULL AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US', PRIMARY KEY(id));
+-- creates a table users following these requirements:
+
+-- With these attributes:
+--	id, integer, never null, auto increment and primary key
+--	email, string (255 characters), never null and unique
+--	name, string (255 characters)
+--	country, enumeration of countries: US, CO and TN, never null
+--		(= default will be the first element of the enumeration, here US)
+-- If the table already exists, your script should not fail
+-- Your script can be executed on any database
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US','CO', 'TN') NOT NULL DEFAULT 'US',
+	PRIMARY KEY(id));
