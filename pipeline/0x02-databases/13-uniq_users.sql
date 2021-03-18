d840fb0f1ebb5285b30e35fcf6071137331d4e55
shincap8 <shincap8@gmail.com>
2021-03-17 21:08:04 -0500

    Remove space before parenthesis

--- pipeline/0x02-databases/13-uniq_users.sql
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -2,4 +2,4 @@
 --id, integer, never null, auto increment and primary key
 --email, string (255 characters), never null and unique
 --name, string (255 characters)
-CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), PRIMARY KEY(id));
