824fd34d18786d97ad2aca14e055173746f4ec85
shincap8 <shincap8@gmail.com>
2021-03-17 21:10:13 -0500

    change int for integer

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -3,4 +3,4 @@
 --email, string (255 characters), never null and unique
 --name, string (255 characters)
 --country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US', PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS users(id INTEGER NOT NULL AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US', PRIMARY KEY(id));
